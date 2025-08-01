eb85a31b1caef1e27049120629b990c1716a13d6
EmptyBox303 <JasonLu2027@u.northwestern.edu>
2025-08-01 11:35:36 -0400

    completed setup_db

--- setup_db.py
+++ setup_db.py
@@ -1,10 +1,19 @@
 import sqlite3
 
 try:
-    with sqlite3.connect("sql.db") as db:
+    with sqlite3.connect("sql1.db") as db:
         print(f"Opened SQLite database with version {sqlite3.sqlite_version} successfully.")
 except sqlite3.OperationalError as e:
     print("Failed to open database:", e)
 
 cur = db.cursor()
+try:
+    with open("org_setup.sql") as file:
+        db_setup_sql = ''.join(file.readlines())
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+
+cur.executescript(db_setup_sql)
+db.commit()
 
